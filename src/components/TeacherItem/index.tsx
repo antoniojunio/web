ca0194d6ab4f2c4849871877989164ad56521e26
Júnior <antoniojunior@MacBook-Pro-de-Junior.local>
2020-08-05 23:31:12 -0300

    Aula 1

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars0.githubusercontent.com/u/4990826?s=460&u=8bac1f8f08a71686d920d82d8eb2f78f06f79263&v=4" alt="Antonio Junior" />
+        <div>
+          <strong>Antônio Júnior</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+                            <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
