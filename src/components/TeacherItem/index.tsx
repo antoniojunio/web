36b08daeb6616615190b3bd9b6af4fe1ec8dcf3a
junior <juniordsj@gmail.com>
2020-08-08 15:47:07 -0300

    Layout da tela dar aulas

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -24,7 +24,7 @@ function TeacherItem() {
       <footer>
         <p>
           Preço/hora
-                            <strong>R$ 80,00</strong>
+        <strong>R$ 80,00</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp" />
